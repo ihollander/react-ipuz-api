62b5b1834152f68fc4dcc3c601b5885a773a58a7
Ian Hollander <ianholla@gmail.com>
2019-06-27 14:19:54 -0400

    cable settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://gridbuds-api.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = '*'
+  config.action_cable.allowed_request_origins = ['https://gridbuds.netlify.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
